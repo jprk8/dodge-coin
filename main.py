976ae85ce63e3c8c9819c1fe2dd8525692176fe5
John Park <176120986+jprk8@users.noreply.github.com>
2025-07-14 17:23:56 -0700

    Add coins for points

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ class DodgeCoin:
         self.coin = pygame.image.load("coin.png")
         self.coins = []
 
+        self.points = 0
+        self.game_font = pygame.font.SysFont("Arial", 24)
+
         self.clock = pygame.time.Clock()
         self.running = True
 
@@ -31,6 +34,11 @@ class DodgeCoin:
         x = randint(0, self.width - self.monster.get_width())
         y = randint(-self.height, 0)
         self.monsters.append([x, y])
+    
+    def spawn_coins(self):
+        x = randint(0, self.width - self.monster.get_width())
+        y = randint(-self.height, 0)
+        self.coins.append([x, y])
 
     def update_monsters(self):
         if randint(1, 30) == 1:
@@ -40,13 +48,29 @@ class DodgeCoin:
         for pos in self.monsters:
             x, y = pos
             if self.check_collision(x, y):
-                self.running = False
+                self.points = 0
                 return
             y += 5
             if y < self.height - self.monster.get_height():
                 new_monsters.append([x, y])
         
         self.monsters = new_monsters
+
+    def update_coins(self):
+        if randint(1, 200) == 1:
+            self.spawn_coins()
+        
+        new_coins = []
+        for pos in self.coins:
+            x, y = pos
+            if self.check_collision(x, y):
+                self.points += 1
+                y += self.height
+            y += 5
+            if y < self.height - self.coin.get_height():
+                new_coins.append([x, y])
+        
+        self.coins = new_coins
 
     def check_collision(self, x, y):
         return (
@@ -71,10 +95,6 @@ class DodgeCoin:
 
             if event.type == pygame.QUIT:
                 self.running = False
-
-    def update(self):
-        self.move_robot()
-        self.update_monsters()
         
     def move_robot(self):
         if self.to_left:
@@ -84,11 +104,20 @@ class DodgeCoin:
         self.robot_x = max(self.robot_x, 0)
         self.robot_x = min(self.robot_x, self.width - self.robot.get_width())
 
+    def update(self):
+        self.move_robot()
+        self.update_monsters()
+        self.update_coins()
+
     def draw(self):
         self.window.fill((30, 30, 30))
         self.window.blit(self.robot, (self.robot_x, self.robot_y))
         for x, y in self.monsters:
             self.window.blit(self.monster, (x, y))
+        for x, y in self.coins:
+            self.window.blit(self.coin, (x, y))
+        score = self.game_font.render(f"Coins: {self.points}", True, (255, 0, 0))
+        self.window.blit(score, (500, 0))
         pygame.display.flip()
 
     def main_loop(self):
